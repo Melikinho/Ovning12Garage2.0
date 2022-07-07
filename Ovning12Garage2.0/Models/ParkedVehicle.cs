f6e1b177c77c0dc281716421847fa7f36a6df7a2
Melikinho <Jimmymelik10@gmail.com>
2022-07-07 16:29:19 +0200

    Setting Properties in class ParkedVehicle

---
+++ Ovning12Garage2.0/Models/ParkedVehicle.cs
@@ -0,0 +1,14 @@
+﻿namespace Ovning12Garage2._0.Models
+{
+    public class ParkedVehicle
+    {
+        public string licenseNumber { get; set; }
+
+        public int numberOfWheels { get; set; }
+
+        public int timeOfArrival { get; set; }
+
+        public string modelVehicle { get; set; }    
+
+    }
+}
