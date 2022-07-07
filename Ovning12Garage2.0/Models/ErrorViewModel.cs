a00403054e0a248f4e18fc17758de3825c566c39
Melikinho <Jimmymelik10@gmail.com>
2022-07-07 16:12:48 +0200

    Add project files.

---
+++ Ovning12Garage2.0/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ovning12Garage2._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
