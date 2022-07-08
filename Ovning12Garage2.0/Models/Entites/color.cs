d20a72899e3d27fd2811b4aa14373f3c7caee32d
Melikinho <Jimmymelik10@gmail.com>
2022-07-08 09:42:35 +0200

    Added enum vehicle values

---
+++ Ovning12Garage2.0/Models/Entites/color.cs
@@ -0,0 +1,13 @@
+﻿namespace Ovning12Garage2._0.Models.Entites
+{
+    public enum Color
+    {
+        Black,
+        Red,
+        Yellow,
+        White,
+        Orange,
+        Green,
+        Blue
+    }
+}
