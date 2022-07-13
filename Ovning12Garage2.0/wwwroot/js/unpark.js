68905e7b2e111d8ac95e57940fe4b16c8ca7e903
Melikinho <Jimmymelik10@gmail.com>
2022-07-13 10:25:34 +0200

    Adjusting Receipt layout

--- Ovning12Garage2.0/wwwroot/js/unpark.js
+++
@@ -1,9 +0,0 @@
-﻿document.querySelector('#unpark').addEventListener('click', function (e) {
-    console.log(e);
-    e.preventDefault();
-
-    var id = document.querySelector('#Id').value;
-    console.log(id);
-
-    fetch('')
-});
